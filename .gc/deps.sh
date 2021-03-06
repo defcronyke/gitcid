ab18fd79ef7853155cf201efcd8e5eaa0f6e438f
Jeremy Carter <jeremy@jeremycarter.ca>
2021-03-05 23:07:23 -0500

    Create new git repos: .gc/init.sh [./repo.git | user@remote:~/repo.git]
    By default they will be bare repos suitable for hosting
    git remotes.

--- .gc/deps.sh
+++ .gc/deps.sh
@@ -1,144 +1,141 @@
 #!/usr/bin/env bash
 
 gitcid_detect_sudo() {
-    SUDO_GROUPS=${SUDO_GROUPS:-"sudo|wheel"}
-    SUDO_CMD=${SUDO_CMD:-""}
-    GROUP_FILE_PATH=${GROUP_FILE_PATH:-"/etc/group"}
-    WHOAMI_CMD=${WHOAMI_CMD:-"whoami"}
+	SUDO_GROUPS=${SUDO_GROUPS:-"sudo|wheel"}
+	SUDO_CMD=${SUDO_CMD:-""}
+	GROUP_FILE_PATH=${GROUP_FILE_PATH:-"/etc/group"}
+	WHOAMI_CMD=${WHOAMI_CMD:-"whoami"}
 
-    cat "$GROUP_FILE_PATH" | grep -P "$SUDO_GROUPS" | grep "$("$WHOAMI_CMD")" >/dev/null
-    if [ $? -eq 0 ]; then
-        gc_log_info "You seem to have sudo privileges. Enabling the sudo command."
-        SUDO_CMD=$(if [ -z "$SUDO_CMD" ]; then echo "sudo"; fi)
-    fi
+	cat "$GROUP_FILE_PATH" | grep -P "$SUDO_GROUPS" | grep "$("$WHOAMI_CMD")" >/dev/null
+	if [ $? -eq 0 ]; then
+		gitcid_log_info ${BASH_SOURCE[0]} $LINENO "You seem to have sudo privileges. Enabling the sudo command."
+		SUDO_CMD=$(if [ -z "$SUDO_CMD" ]; then echo "sudo"; fi)
+	fi
 }
 
 gitcid_detect_os() {
-    gc_log_info "Attempting to find any missing GitCid dependencies and install them."
-    
-    ARCH_PKG_CMD=${ARCH_PKG_CMD:-"pacman"}
-    ARCH_PKG_CMD_INSTALL_ARGS=${ARCH_PKG_CMD_INSTALL_ARGS:-"--noconfirm -Syy"}
+	gitcid_log_info ${BASH_SOURCE[0]} $LINENO "Attempting to find any missing GitCid dependencies and install them."
+	
+	ARCH_PKG_CMD=${ARCH_PKG_CMD:-"pacman"}
+	ARCH_PKG_CMD_INSTALL_ARGS=${ARCH_PKG_CMD_INSTALL_ARGS:-"--noconfirm -Syy"}
 
-    DEBIAN_PKG_CMD=${DEBIAN_PKG_CMD:-"apt-get"}
-    DEBIAN_PKG_CMD_UPDATE_ARGS=${DEBIAN_PKG_CMD_UPDATE_ARGS:-"update"}
-    DEBIAN_PKG_CMD_INSTALL_ARGS=${DEBIAN_PKG_CMD_INSTALL_ARGS:-"install --no-install-recommends -y"}
+	DEBIAN_PKG_CMD=${DEBIAN_PKG_CMD:-"apt-get"}
+	DEBIAN_PKG_CMD_UPDATE_ARGS=${DEBIAN_PKG_CMD_UPDATE_ARGS:-"update"}
+	DEBIAN_PKG_CMD_INSTALL_ARGS=${DEBIAN_PKG_CMD_INSTALL_ARGS:-"install --no-install-recommends -y"}
 
-    FEDORA_PKG_CMD=${FEDORA_PKG_CMD:-"dnf"}
-    FEDORA_PKG_CMD_UPDATE_ARGS=${FEDORA_PKG_CMD_UPDATE_ARGS:-"-y update"}
-    FEDORA_PKG_CMD_INSTALL_ARGS=${FEDORA_PKG_CMD_INSTALL_ARGS:-"-y install"}
+	FEDORA_PKG_CMD=${FEDORA_PKG_CMD:-"dnf"}
+	FEDORA_PKG_CMD_UPDATE_ARGS=${FEDORA_PKG_CMD_UPDATE_ARGS:-"-y update"}
+	FEDORA_PKG_CMD_INSTALL_ARGS=${FEDORA_PKG_CMD_INSTALL_ARGS:-"-y install"}
 
-    FEDORA_OLD_PKG_CMD=${FEDORA_OLD_PKG_CMD:-"yum"}
-    FEDORA_OLD_PKG_CMD_UPDATE_ARGS=${FEDORA_OLD_PKG_CMD_UPDATE_ARGS:-"-y update"}
-    FEDORA_OLD_PKG_CMD_INSTALL_ARGS=${FEDORA_OLD_PKG_CMD_INSTALL_ARGS:-"-y install"}
+	FEDORA_OLD_PKG_CMD=${FEDORA_OLD_PKG_CMD:-"yum"}
+	FEDORA_OLD_PKG_CMD_UPDATE_ARGS=${FEDORA_OLD_PKG_CMD_UPDATE_ARGS:-"-y update"}
+	FEDORA_OLD_PKG_CMD_INSTALL_ARGS=${FEDORA_OLD_PKG_CMD_INSTALL_ARGS:-"-y install"}
 
-    which $ARCH_PKG_CMD >/dev/null 2>&1
-    IS_ARCH=$?
+	which $ARCH_PKG_CMD >/dev/null 2>&1
+	IS_ARCH=$?
 
-    which $DEBIAN_PKG_CMD >/dev/null 2>&1
-    IS_DEBIAN=$?
+	which $DEBIAN_PKG_CMD >/dev/null 2>&1
+	IS_DEBIAN=$?
 
-    which $FEDORA_PKG_CMD >/dev/null 2>&1
-    IS_FEDORA=$?
+	which $FEDORA_PKG_CMD >/dev/null 2>&1
+	IS_FEDORA=$?
 
-    which $FEDORA_OLD_PKG_CMD >/dev/null 2>&1
-    IS_FEDORA_OLD=$?
+	which $FEDORA_OLD_PKG_CMD >/dev/null 2>&1
+	IS_FEDORA_OLD=$?
 
-    SUPPORTED_DISTRO=0
+	SUPPORTED_DISTRO=0
 
-    if [ $IS_ARCH -eq 0 ]; then
-        source "${GITCID_DEPS_DIR}arch-deps.env"
-    elif [ $IS_DEBIAN -eq 0 ]; then
-        source "${GITCID_DEPS_DIR}debian-deps.env"
-    elif [ $IS_FEDORA -eq 0 ]; then
-        source "${GITCID_DEPS_DIR}fedora-deps.env"
-    elif [ $IS_FEDORA_OLD -eq 0 ]; then
-        source "${GITCID_DEPS_DIR}fedora-old-deps.env"
-    else
-        SUPPORTED_DISTRO=1
-        source "${GITCID_DEPS_DIR}unsupported-deps.env"
-        gc_log_warn "Your OS isn't supported. This still might work if you have the following \
+	if [ $IS_ARCH -eq 0 ]; then
+		source "${GITCID_DEPS_DIR}arch-deps.env"
+	elif [ $IS_DEBIAN -eq 0 ]; then
+		source "${GITCID_DEPS_DIR}debian-deps.env"
+	elif [ $IS_FEDORA -eq 0 ]; then
+		source "${GITCID_DEPS_DIR}fedora-deps.env"
+	elif [ $IS_FEDORA_OLD -eq 0 ]; then
+		source "${GITCID_DEPS_DIR}fedora-old-deps.env"
+	else
+		SUPPORTED_DISTRO=1
+		source "${GITCID_DEPS_DIR}unsupported-deps.env"
+		gitcid_log_warn ${BASH_SOURCE[0]} $LINENO "Your OS isn't supported. This still might work if you have the following \
 dependencies installed:"
-        echo "${GITCID_DEPS[@]}"
-    fi
+		echo "${GITCID_DEPS[@]}"
+	fi
 }
 
 gitcid_deps() {
-    echo "$(date -Ins)  info: Running script: $0 $@"
+	echo "$(date -Ins) [${BASH_SOURCE[0]} - line: $LINENO]	info: Running script: ${BASH_SOURCE[0]} $@"
 
-    GITCID_DIR=${GITCID_DIR:-"./.gc/"}
-    GITCID_DEPS_DIR=${GITCID_DEPS_DIR:-"${GITCID_DIR}.gc-deps/"}
-    GITCID_GIT_HOOKS_CLIENT_DIR=${GITCID_GIT_HOOKS_CLIENT_DIR:-"${GITCID_DIR}.gc-git-hooks-client"}
-    GITCID_UTIL_DIR=${GITCID_UTIL_DIR:-"${GITCID_DIR}.gc-util/"}
-    GITCID_UTIL_LOG=${GITCID_UTIL_LOG:-"${GITCID_UTIL_DIR}log.env"}
+	GITCID_DIR=${GITCID_DIR:-".gc/"}
+	GITCID_DEPS_DIR=${GITCID_DEPS_DIR:-"${GITCID_DIR}.gc-deps/"}
+	GITCID_GIT_HOOKS_CLIENT_DIR=${GITCID_GIT_HOOKS_CLIENT_DIR:-"${GITCID_DIR}.gc-git-hooks-client"}
+	GITCID_UTIL_DIR=${GITCID_UTIL_DIR:-"${GITCID_DIR}.gc-util/"}
+	GITCID_UTIL_LOG=${GITCID_UTIL_LOG:-"${GITCID_UTIL_DIR}log.env"}
 
-    echo "$(date -Ins)  info: Importing GitCid log utils: ${GITCID_UTIL_LOG}"
-    source "${GITCID_UTIL_LOG}"
-    res_import_util_log=$?
-    if [ $res_import_util_log -ne 0 ]; then
-        echo "$(date -Ins)  error: Failed importing GitCid log utils. I guess it's not going to work, sorry!"
-        return $res_import_util_log
-    fi
+	echo "$(date -Ins) [${BASH_SOURCE[0]} - line: $LINENO]	import: Importing GitCid log utils: ${GITCID_UTIL_LOG}"
+	source "${GITCID_UTIL_LOG}"
+	res_import_util_log=$?
+	if [ $res_import_util_log -ne 0 ]; then
+		echo "$(date -Ins) [${BASH_SOURCE[0]} - line: $LINENO]	error: Failed importing GitCid log utils. I guess it's not going to work, sorry!"
+		return $res_import_util_log
+	fi
 
-    gitcid_detect_sudo $@
-    res_detect_sudo=$?
-    if [ $res_detect_sudo -ne 0 ]; then
-        gc_log_warn "GitCid failed detecting if you have sudo privileges. We will assume you don't, \
+	gitcid_detect_sudo $@
+	res_detect_sudo=$?
+	if [ $res_detect_sudo -ne 0 ]; then
+		gitcid_log_warn ${BASH_SOURCE[0]} $LINENO "GitCid failed detecting if you have sudo privileges. We will assume you don't, \
 so you'll need to run this script as root. It will probably fail now if you aren't root already."
-    fi
+	fi
 
-    gitcid_detect_os $@
-    res_detect_os=$?
-    if [ $res_detect_os -ne 0 ]; then
-        gc_log_err "GitCid failed detecting your OS. I guess it's not going to work, sorry!"
-        return $res_detect_os
-    fi
+	gitcid_detect_os $@
+	res_detect_os=$?
+	if [ $res_detect_os -ne 0 ]; then
+		gitcid_log_err ${BASH_SOURCE[0]} $LINENO "GitCid failed detecting your OS. I guess it's not going to work, sorry!"
+		return $res_detect_os
+	fi
 
-    HAS_DEPS=0
-    for i in ${GITCID_DEPS_CMDS[@]}; do
-        which $i >/dev/null 2>&1
-        HAS_DEPS=$?
-    done
+	HAS_DEPS=0
+	for i in ${GITCID_DEPS_CMDS[@]}; do
+		which $i >/dev/null 2>&1
+		HAS_DEPS=$?
+	done
 
-    if [ $HAS_DEPS -ne 0 ]; then
-        gc_log_info "You are missing at least one of these dependencies:"
-        echo "${GITCID_DEPS[@]}"
-        echo ""
-        
-        if [ $SUPPORTED_DISTRO -eq 0 ]; then
-            gc_log_info "We will try to install them now, using the following command:"
-            echo "${GITCID_DEPS_INSTALL_CMD[@]} ${GITCID_DEPS[@]}"
-            echo ""
+	if [ $HAS_DEPS -ne 0 ]; then
+		gitcid_log_info ${BASH_SOURCE[0]} $LINENO "You are missing at least one of these dependencies:"
+		echo "${GITCID_DEPS[@]}"
+		
+		if [ $SUPPORTED_DISTRO -eq 0 ]; then
+			gitcid_log_info ${BASH_SOURCE[0]} $LINENO "We will try to install them now, using the following command:"
+			echo "${GITCID_DEPS_INSTALL_CMD[@]} ${GITCID_DEPS[@]}"
 
-            eval "${GITCID_DEPS_INSTALL_CMD[@]} ${GITCID_DEPS[@]}"
-            res=$?
+			eval "${GITCID_DEPS_INSTALL_CMD[@]} ${GITCID_DEPS[@]}"
+			res=$?
 
-            if [ $res -ne 0 ]; then
-                gc_log_err "Failed installing dependencies. You'll need to install them manually then I guess."
-                return 2
-            fi
-        else
-            gc_log_err "You are missing some dependencies and your OS isn't supported. \
+			if [ $res -ne 0 ]; then
+				gitcid_log_err ${BASH_SOURCE[0]} $LINENO "Failed installing dependencies. You'll need to install them manually then I guess."
+				return 2
+			fi
+		else
+			gitcid_log_err ${BASH_SOURCE[0]} $LINENO "You are missing some dependencies and your OS isn't supported. \
 Please install them yourself and try again afterwards. Maybe it'll work if you do that."
-            gc_log_err "You can also try setting the following environment variables to \
+			gitcid_log_err ${BASH_SOURCE[0]} $LINENO "You can also try setting the following environment variables to \
 please our system, if you know the correct values for your unsupported OS:"
-            echo "GITCID_DEPS_INSTALL_CMD=${GITCID_DEPS_INSTALL_CMD[@]}"
-            echo "GITCID_DEPS=${GITCID_DEPS[@]}"
-            echo "GITCID_DEPS_CMDS=${GITCID_DEPS_CMDS[@]}"
-            echo ""
-            return 1
-        fi
-    fi
+			echo "GITCID_DEPS_INSTALL_CMD=${GITCID_DEPS_INSTALL_CMD[@]}"
+			echo "GITCID_DEPS=${GITCID_DEPS[@]}"
+			echo "GITCID_DEPS_CMDS=${GITCID_DEPS_CMDS[@]}"
+			return 1
+		fi
+	fi
 
-    gc_log_info "Setting \"$GITCID_GIT_HOOKS_CLIENT_DIR\" as this git repo's \"core.hooksPath\""
-    
-    git config core.hooksPath "$GITCID_GIT_HOOKS_CLIENT_DIR"
-    git_config_res=$?
-    if [ $git_config_res -ne 0 ]; then
-        gc_log_err "Failed setting this git repo's \"core.hooksPath\". I guess this isn't going to work, sorry!"
-        return $git_config_res
-    fi
+	gitcid_log_info ${BASH_SOURCE[0]} $LINENO "Setting \"$GITCID_GIT_HOOKS_CLIENT_DIR\" as this git repo's \"core.hooksPath\""
+	
+	git config core.hooksPath "$GITCID_GIT_HOOKS_CLIENT_DIR"
+	git_config_res=$?
+	if [ $git_config_res -ne 0 ]; then
+		gitcid_log_err ${BASH_SOURCE[0]} $LINENO "Failed setting this git repo's \"core.hooksPath\". I guess this isn't going to work, sorry!"
+		return $git_config_res
+	fi
 
-    gc_log_info "All GitCid dependencies are installed."
+	gitcid_log_info ${BASH_SOURCE[0]} $LINENO "All GitCid dependencies are installed."
 }
 
 gitcid_deps $@
---
+++ .gc/init.sh
@@ -0,0 +1,193 @@
+#!/usr/bin/env bash
+
+gitcid_get_project_version() {
+	GITCID_VERSION_DEFAULT=${GITCID_VERSION_DEFAULT:-"v0.1.0"}
+	GITCID_VERSION="${GITCID_VERSION:-$(cat "${GITCID_DIR}.gc-version.txt" | xargs)}"
+	if [ $? -ne 0 ]; then
+		GITCID_VERSION="$GITCID_VERSION_DEFAULT"
+	fi
+
+	echo "$GITCID_VERSION"
+}
+
+gitcid_get_project_link() {
+	echo "https://gitlab.com/defcronyke/gitcid"
+}
+
+gitcid_get_project_author() {
+	echo "Copyright (c) 2021 Jeremy Carter <jeremy@jeremycarter.ca>"
+}
+
+gitcid_get_project_license() {
+	echo "MIT License: https://gitlab.com/defcronyke/gitcid/-/blob/master/LICENSE"
+}
+
+gitcid_get_init_name() {
+	echo "GitCid $(gitcid_get_project_version) ${BASH_SOURCE[0]}"
+}
+
+gitcid_get_init_header() {
+	echo "$(gitcid_get_init_name)"
+	echo "------"
+	echo "$(gitcid_get_project_author)"
+	echo "$(gitcid_get_project_license)"
+	echo "Website: $(gitcid_get_project_link)"
+	echo "------"
+	echo ""
+	echo "Description: ${BASH_SOURCE[0]} - Initialize a new git repository (or several), either locally or at an ssh server path. Defaults to making a bare repo suitable for hosting a git remote."
+	echo ""
+	echo "Summary: ${BASH_SOURCE[0]} [-h | --help] [./repo[.git] ...] [[user@]remote:~/repo[.git] ...]"
+	echo ""
+	echo "You can override the following environment variables if you want:"
+	echo "GITCID_NEW_REPO_BARE=\"${GITCID_NEW_REPO_BARE}\""
+	echo "GITCID_NEW_REPO_PERMISSIONS=\"${GITCID_NEW_REPO_PERMISSIONS}\""
+	echo "GITCID_NEW_REPO_PATH_DEFAULT=\"${GITCID_NEW_REPO_PATH_DEFAULT}\""
+	echo "GITCID_DIR=\"${GITCID_DIR}\""
+	echo ""
+	echo "------"
+}
+
+gitcid_get_init_usage() {
+	echo "$(gitcid_get_init_header)"
+	echo ""
+	echo "Usage: ${BASH_SOURCE[0]} [ OPTION ... ] [ ARGUMENT ... ]"
+	echo ""
+	echo "Example: ${BASH_SOURCE[0]} ./new-local-repo.git user@some-ssh-server:~/new-remote-repo.git"
+	echo ""
+	echo "With no arguments provided, it defaults to this command: ${BASH_SOURCE[0]} ${GITCID_NEW_REPO_PATH_DEFAULT}"
+	echo ""
+	echo "OPTIONS"
+	echo "-------"
+	echo "[ -h | --help ]"
+	echo ""
+	echo "ARGUMENT EXAMPLES"
+	echo "-----------------"
+	echo "[ repo[.git] ... ]"
+	echo "[ ./rel/path/to/new/local/repo[.git] ... ]"
+	echo "[ /abs/path/to/new/local/repo[.git] ... ]"
+	echo "[ user@host:~/rel/ssh/path/to/new/remote/repo[.git] ... ]"
+	echo "[ user@host:/abs/ssh/path/to/new/remote/repo[.git] ... ]"
+	return 1
+}
+
+gitcid_make_new_git_repo() {
+	GITCID_NEW_REPO_PATH=${1:-"${GITCID_NEW_REPO_PATH_DEFAULT}/${GITCID_NEW_REPO_NAME_DEFAULT}"}
+	GITCID_NEW_REPO_PATHS=("${GITCID_NEW_REPO_PATH}")
+	GITCID_GIT_INIT_SHARED_DOCS_URL="https://git-scm.com/docs/git-init#Documentation/git-init.txt---sharedfalsetrueumaskgroupallworldeverybody0xxx"
+
+	if [ $# -le 0 ]; then
+		gitcid_log_info ${BASH_SOURCE[0]} $LINENO "No path argument provided for the new git repo. Using default local path: \"$GITCID_NEW_REPO_PATH\""
+	else
+		GITCID_NEW_REPO_PATHS=("$@")
+	fi
+	
+	gitcid_log_info ${BASH_SOURCE[0]} $LINENO "GITCID_NEW_REPO_PATHS=(${GITCID_NEW_REPO_PATHS[@]})"
+
+	for new_repo_in_path in "${GITCID_NEW_REPO_PATHS[@]}"; do
+		GITCID_NEW_REPO_PATH="$(echo "${new_repo_in_path}" | rev | cut -d'/' -f2- | rev)"
+		GITCID_NEW_REPO_NAME="$(echo "${new_repo_in_path}" | rev | cut -d'/' -f1 | rev)"
+
+		echo "${new_repo_in_path}" | grep "/" >/dev/null
+		if [ $? -ne 0 ]; then
+			gitcid_log_info ${BASH_SOURCE[0]} $LINENO "The new repo name has no path, it just has a name. Assuming we want to make it in the current working directory."
+			GITCID_NEW_REPO_PATH="."
+		fi
+
+		GITCID_NEW_REPO_SUFFIX=${GITCID_NEW_REPO_SUFFIX:-".git"}
+
+		echo "$GITCID_NEW_REPO_NAME" | grep -P "^.+\\${GITCID_NEW_REPO_SUFFIX}$" >/dev/null
+		if [ $? -ne 0 ]; then
+			GITCID_NEW_REPO_NAME="${GITCID_NEW_REPO_NAME}${GITCID_NEW_REPO_SUFFIX}"
+			gitcid_log_info ${BASH_SOURCE[0]} $LINENO "Adding \"${GITCID_NEW_REPO_SUFFIX}\" to the end of the new repo name: ${GITCID_NEW_REPO_NAME}"
+		fi
+		
+		gitcid_log_info ${BASH_SOURCE[0]} $LINENO "New git repo permissions (for the \"git init --shared\" option): ${GITCID_NEW_REPO_PERMISSIONS}"
+		gitcid_log_info ${BASH_SOURCE[0]} $LINENO "See the docs for \"git init --shared\" for more possible settings:"
+		echo "${GITCID_GIT_INIT_SHARED_DOCS_URL}"
+
+		echo "$GITCID_NEW_REPO_PATH" | grep -P ".*@*.+:.+" >/dev/null
+		GITCID_IS_SSH_PATH=$?
+
+		if [ $GITCID_IS_SSH_PATH -eq 0 ]; then
+			gitcid_log_info ${BASH_SOURCE[0]} $LINENO "Initializing new git repo at ssh destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+
+			GITCID_NEW_REPO_PATH_HOST=$(echo "$GITCID_NEW_REPO_PATH" | cut -d':' -f1)
+			GITCID_NEW_REPO_PATH_DIR=$(echo "$GITCID_NEW_REPO_PATH" | cut -d':' -f2)
+
+			output_git_init="$(ssh "$GITCID_NEW_REPO_PATH_HOST" "git init --shared=${GITCID_NEW_REPO_PERMISSIONS} ${GITCID_NEW_REPO_BARE} ${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME}" 2>&1)"
+			res_git_init=$?
+			if [ $res_git_init -ne 0 ]; then
+				gitcid_log_err ${BASH_SOURCE[0]} $LINENO "$output_git_init"
+				gitcid_log_err ${BASH_SOURCE[0]} $LINENO "Failed initializing new remote git repo: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+				return $res_git_init
+			else
+				gitcid_log_info ${BASH_SOURCE[0]} $LINENO "$output_git_init"
+			fi
+
+			gitcid_log_info ${BASH_SOURCE[0]} $LINENO "New git repo initialized at remote destination: ${GITCID_NEW_REPO_PATH_HOST}:${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME}"
+		else
+			gitcid_log_info ${BASH_SOURCE[0]} $LINENO "Initializing new git repo at local destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+
+			output_git_init="$(git init --shared=${GITCID_NEW_REPO_PERMISSIONS} ${GITCID_NEW_REPO_BARE} "${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}" 2>&1)"
+			res_git_init=$?
+			if [ $res_git_init -ne 0 ]; then
+				gitcid_log_err ${BASH_SOURCE[0]} $LINENO "$output_git_init"
+				gitcid_log_err ${BASH_SOURCE[0]} $LINENO "Failed initializing new local git repo: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+				return $res_git_init
+			else
+				gitcid_log_info ${BASH_SOURCE[0]} $LINENO "$output_git_init"
+			fi
+
+			gitcid_log_info ${BASH_SOURCE[0]} $LINENO "New git repo initialized at local destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+		fi
+	done
+}
+
+gitcid_init() {
+	GITCID_DIR=${GITCID_DIR:-".gc/"}
+	GITCID_NEW_REPO_BARE=${GITCID_NEW_REPO_BARE:-"--bare"}
+	GITCID_NEW_REPO_PERMISSIONS=${GITCID_NEW_REPO_PERMISSIONS:-"0640"}
+	GITCID_NEW_REPO_PATH_DEFAULT=${GITCID_NEW_REPO_PATH_DEFAULT:-"."}
+	GITCID_NEW_REPO_NAME_DEFAULT=${GITCID_NEW_REPO_NAME_DEFAULT:-"repo.git"}
+
+	if [[ $# -ge 1 && ("$1" == "-h" || "$1" == "--help") ]]; then
+		shift
+		gitcid_get_init_usage $@
+		return $?
+	fi
+
+	if [[ $# -ge 1 && ("$1" == "-V" || "$1" == "--version") ]]; then
+		shift
+		gitcid_get_project_version $@
+		return $?
+	fi
+
+	if [[ $# -ge 1 && ("$1" == "-n" || "$1" == "--name") ]]; then
+		shift
+		gitcid_get_init_name $@
+		return $?
+	fi
+
+	gitcid_get_init_header
+
+	echo ""
+	echo "Logs"
+	echo "----"
+	echo "$(date -Ins) [${BASH_SOURCE[0]} - line: $LINENO]	info: Running script: ${BASH_SOURCE[0]} $@"
+
+	source "${GITCID_DIR}deps.sh"
+	res_import_deps=$?
+	if [ $res_import_deps -ne 0 ]; then
+		gitcid_log_err ${BASH_SOURCE[0]} $LINENO "Failed importing GitCid dependencies. I guess it's not going to work, sorry!"
+		return $res_import_deps
+	fi
+
+	gitcid_make_new_git_repo $@
+	res_make_new_git_repo=$?
+	if [ $res_make_new_git_repo -ne 0 ]; then
+		gitcid_log_err ${BASH_SOURCE[0]} $LINENO "Failed making new git repo. I guess it's not going to work, sorry!"
+		return $res_make_new_git_repo
+	fi
+}
+
+gitcid_init $@
