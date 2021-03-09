0b73a5f81f78f89f3ef64cf000d0bd91727d186d
Jeremy Carter <jeremy@jeremycarter.ca>
2021-03-09 12:08:25 -0500

    Update Dockerfile.

--- .gc/.gc-docker-compose/hello/up.sh
+++ .gc/.gc-docker-compose/hello/up.sh
@@ -1,11 +1,40 @@
 #!/usr/bin/env bash
 
-pwd="$PWD"
-cd "$(dirname "${BASH_SOURCE[0]}")"
+gitcid_get_architecture() {
+	uname -a | grep "x86_64" >/dev/null
+	IS_X64=$?
 
-docker network create docker-compose-net --subnet 10.0.1.0/24 2>/dev/null
+	uname -a | grep "arm64" >/dev/null
+	IS_ARM64=$?
 
-docker-compose up $@
-docker-compose down $@
+	uname -a | grep "arm" >/dev/null
+	IS_ARMHF=$?
 
-cd "$PWD"
+	if [ $IS_X64 -eq 0 ]; then
+		ARCH=""
+	elif [ $IS_ARM64 -eq 0 ]; then
+		ARCH="arm64v8/"
+	elif [ $IS_ARMHF -eq 0 ]; then
+		ARCH="arm32v7/"
+	else
+		ARCH=""
+	fi
+}
+
+gitcid_docker_compose_hello_up() {
+	gitcid_get_architecture $@
+
+	pwd="$PWD"
+	cd "$(dirname "${BASH_SOURCE[0]}")"
+
+	docker network create docker-compose-net --subnet 10.0.1.0/24 2>/dev/null
+
+	cat Dockerfile.tmpl | sed "s/{ARCH}/${ARCH}/g" | tee Dockerfile
+
+	docker-compose up $@
+	docker-compose down $@
+
+	cd "$PWD"
+}
+
+gitcid_docker_compose_hello_up $@
