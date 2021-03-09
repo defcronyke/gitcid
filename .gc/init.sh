3607c2479313bbeb6f0635535ad0ee63112fc801
Jeremy Carter <jeremy@jeremycarter.ca>
2021-03-08 22:53:18 -0500

    Add .gc/.gc-init-template/

--- .gc/init.sh
+++ .gc/init.sh
@@ -74,86 +74,6 @@ gitcid_get_init_usage() {
 	echo "[ user@host:/abs/ssh/path/to/new/remote/repo[.git] ... ]"
 }
 
-gitcid_make_new_git_repo() {
-	GITCID_NEW_REPO_PATH=${1:-"${GITCID_NEW_REPO_PATH_DEFAULT}"}
-	GITCID_NEW_REPO_PATHS=("${GITCID_NEW_REPO_PATH}")
-	GITCID_GIT_INIT_SHARED_DOCS_URL="https://git-scm.com/docs/git-init#Documentation/git-init.txt---sharedfalsetrueumaskgroupallworldeverybody0xxx"
-
-	if [ $# -le 0 ]; then
-		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "No path argument provided for the new git repo. Using default local path: \"$GITCID_NEW_REPO_PATH\""
-	
-	else
-		GITCID_NEW_REPO_PATHS=("$@")
-	fi
-	
-	gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "GITCID_NEW_REPO_PATHS=(${GITCID_NEW_REPO_PATHS[@]})"
-
-	for new_repo_in_path in "${GITCID_NEW_REPO_PATHS[@]}"; do
-		GITCID_NEW_REPO_PATH="$(echo "${new_repo_in_path}" | rev | cut -d'/' -f2- | rev)"
-		GITCID_NEW_REPO_NAME="$(echo "${new_repo_in_path}" | rev | cut -d'/' -f1 | rev)"
-
-		echo "${new_repo_in_path}" | grep "/" >/dev/null
-		if [ $? -ne 0 ]; then
-			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "The new repo name has no path, it just has a name. Assuming we want to make it in the current working directory."
-			GITCID_NEW_REPO_PATH="."
-		fi
-
-		GITCID_NEW_REPO_SUFFIX=${GITCID_NEW_REPO_SUFFIX:-".git"}
-
-		echo "$GITCID_NEW_REPO_NAME" | grep -P "^.+\\${GITCID_NEW_REPO_SUFFIX}$" >/dev/null
-		if [ $? -ne 0 ] && [ -z ${GITCID_NEW_REPO_NON_BARE+x} ]; then
-			GITCID_NEW_REPO_NAME="${GITCID_NEW_REPO_NAME}${GITCID_NEW_REPO_SUFFIX}"
-			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "Adding \"${GITCID_NEW_REPO_SUFFIX}\" to the end of the new repo name: ${GITCID_NEW_REPO_NAME}"
-		fi
-		
-		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "New git repo permissions (for the \"git init --shared\" option): ${GITCID_NEW_REPO_PERMISSIONS}"
-		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "See the docs for \"git init --shared\" for more possible settings:"
-		gitcid_log_echo "${BASH_SOURCE[0]}" $LINENO "${GITCID_GIT_INIT_SHARED_DOCS_URL}"
-
-		echo "$GITCID_NEW_REPO_PATH" | grep -P ".*@*.+:.+" >/dev/null
-		GITCID_IS_SSH_PATH=$?
-
-		if [ $GITCID_IS_SSH_PATH -eq 0 ]; then
-			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "Initializing new git repo at ssh destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
-
-			GITCID_NEW_REPO_PATH_HOST=$(echo "$GITCID_NEW_REPO_PATH" | cut -d':' -f1)
-			GITCID_NEW_REPO_PATH_DIR=$(echo "$GITCID_NEW_REPO_PATH" | cut -d':' -f2)
-
-			output_git_init="$(ssh "$GITCID_NEW_REPO_PATH_HOST" "git init --shared=${GITCID_NEW_REPO_PERMISSIONS} ${GITCID_NEW_REPO_BARE} ${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME}" 2>&1)"
-			res_git_init=$?
-			if [ $res_git_init -ne 0 ]; then
-				gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "$output_git_init"
-				gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "Failed initializing new remote git repo: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
-				
-				return $res_git_init
-			
-			else
-				gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "$output_git_init"
-			fi
-
-			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "New git repo initialized at remote destination: ${GITCID_NEW_REPO_PATH_HOST}:${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME}"
-		
-		else
-			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "Initializing new git repo at local destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
-
-			output_git_init="$(git init --shared=${GITCID_NEW_REPO_PERMISSIONS} ${GITCID_NEW_REPO_BARE} "${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}" 2>&1)"
-			res_git_init=$?
-			if [ $res_git_init -ne 0 ]; then
-				gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "$output_git_init"
-				gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "Failed initializing new local git repo: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
-				
-				return $res_git_init
-			
-			else
-				gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "$output_git_init"
-			fi
-
-			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "New git repo initialized at local destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
-		fi
-
-	done
-}
-
 gitcit_begin_logs() {
 	echo ""
 	echo "Logs"
@@ -223,6 +143,147 @@ gitcid_handle_args() {
 	fi
 }
 
+gitcid_wait_for_background_jobs() {
+	for job_pid in ${GITCID_BACKGROUND_JOBS[@]}; do
+		any_jobs="y"
+		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "Waiting for GitCid background job to finish: $job_pid"
+		wait $job_pid
+	done
+
+	GITCID_BACKGROUND_JOBS=()
+
+	if [ ! -z ${any_jobs+x} ]; then
+		unset any_jobs
+		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "All GitCid background jobs are finished."
+	fi
+}
+
+gitcid_mention_update() {
+	if [ -f "${GITCID_DIR}.gc-update-available" ]; then
+		gitcid_log_update "${BASH_SOURCE[0]}" $LINENO "An updated version of GitCid is available. \
+Please run the following command at your earliest opportunity:\ngit pull" 1>&2
+		return 0
+	fi
+
+	if [ ! -z ${GITCID_CHECKED_FOR_UPDATES+x} ]; then
+		gitcid_log_update_verbose "${BASH_SOURCE[0]}" $LINENO "GitCid is up-to-date." 1>&2
+	fi
+}
+
+gitcid_init_exit() {
+	res=$1
+	shift
+
+	gitcid_wait_for_background_jobs
+
+	gitcid_mention_update
+
+	gitcid_log_notice_verbose "${BASH_SOURCE[0]}" $LINENO "GitCid script finished: $0 $@"
+
+	if [ $res -ne 0 ]; then
+		gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "GitCid script finished with an error.\nexit code:\n$res"
+	else
+		gitcid_log_echo_verbose "${BASH_SOURCE[0]}" $LINENO "exit code:\n$res"
+	fi
+
+	return $res
+}
+
+gitcid_make_new_git_repo() {
+	GITCID_NEW_REPO_PATH=${1:-"${GITCID_NEW_REPO_PATH_DEFAULT}"}
+	GITCID_NEW_REPO_PATHS=("${GITCID_NEW_REPO_PATH}")
+	GITCID_GIT_INIT_SHARED_DOCS_URL="https://git-scm.com/docs/git-init#Documentation/git-init.txt---sharedfalsetrueumaskgroupallworldeverybody0xxx"
+	GITCID_INIT_TEMPLATE_DIR=${GITCID_INIT_TEMPLATE_DIR:-"${GITCID_DIR}.gc-init-template/"}
+
+	if [ $# -le 0 ]; then
+		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "No path argument provided for the new git repo. Using default local path: \"$GITCID_NEW_REPO_PATH\""
+	
+	else
+		GITCID_NEW_REPO_PATHS=("$@")
+	fi
+	
+	gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "GITCID_NEW_REPO_PATHS=(${GITCID_NEW_REPO_PATHS[@]})"
+
+	for new_repo_in_path in "${GITCID_NEW_REPO_PATHS[@]}"; do
+		GITCID_NEW_REPO_PATH="$(echo "${new_repo_in_path}" | rev | cut -d'/' -f2- | rev)"
+		GITCID_NEW_REPO_NAME="$(echo "${new_repo_in_path}" | rev | cut -d'/' -f1 | rev)"
+
+		echo "${new_repo_in_path}" | grep "/" >/dev/null
+		if [ $? -ne 0 ]; then
+			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "The new repo name has no path, it just has a name. Assuming we want to make it in the current working directory."
+			GITCID_NEW_REPO_PATH="."
+		fi
+
+		GITCID_NEW_REPO_SUFFIX=${GITCID_NEW_REPO_SUFFIX:-".git"}
+
+		echo "$GITCID_NEW_REPO_NAME" | grep -P "^.+\\${GITCID_NEW_REPO_SUFFIX}$" >/dev/null
+		if [ $? -ne 0 ] && [ -z ${GITCID_NEW_REPO_NON_BARE+x} ]; then
+			GITCID_NEW_REPO_NAME="${GITCID_NEW_REPO_NAME}${GITCID_NEW_REPO_SUFFIX}"
+			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "Adding \"${GITCID_NEW_REPO_SUFFIX}\" to the end of the new repo name: ${GITCID_NEW_REPO_NAME}"
+		fi
+		
+		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "New git repo permissions (for the \"git init --shared\" option): ${GITCID_NEW_REPO_PERMISSIONS}"
+		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "See the docs for \"git init --shared\" for more possible settings:"
+		gitcid_log_echo "${BASH_SOURCE[0]}" $LINENO "${GITCID_GIT_INIT_SHARED_DOCS_URL}"
+
+		echo "$GITCID_NEW_REPO_PATH" | grep -P ".*@*.+:.+" >/dev/null
+		GITCID_IS_SSH_PATH=$?
+
+		GITCID_NEW_REPO_PATH_DIR=$(echo "$GITCID_NEW_REPO_PATH" | cut -d':' -f2)
+
+		if [ $GITCID_IS_SSH_PATH -eq 0 ]; then
+			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "Initializing new git repo at ssh destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+
+			GITCID_NEW_REPO_PATH_HOST=$(echo "$GITCID_NEW_REPO_PATH" | cut -d':' -f1)
+
+			ssh "$GITCID_NEW_REPO_PATH_HOST" "mkdir -p ${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME}"
+			scp -r "${GITCID_DIR}" $GITCID_NEW_REPO_PATH_HOST:${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME} >/dev/null
+			scp "${GITCID_DIR}../README.md" $GITCID_NEW_REPO_PATH_HOST:${GITCID_NEW_REPO_PATH_DIR}/"${GITCID_NEW_REPO_NAME}/README-gitcid.md" 2>/dev/null || \
+			scp "${GITCID_DIR}../README-gitcid.md" $GITCID_NEW_REPO_PATH_HOST:${GITCID_NEW_REPO_PATH_DIR}/"${GITCID_NEW_REPO_NAME}/README-gitcid.md" 2>/dev/null
+			
+			output_git_init="Running ssh... $(ssh "$GITCID_NEW_REPO_PATH_HOST" \
+"git init --shared=${GITCID_NEW_REPO_PERMISSIONS} --template=${GITCID_INIT_TEMPLATE_DIR} ${GITCID_NEW_REPO_BARE} ${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME}")"
+			
+			res_git_init=$?
+			if [ $res_git_init -ne 0 ]; then
+				gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "$output_git_init"
+				gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "Failed initializing new remote git repo: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+				
+				return $res_git_init
+			
+			else
+				gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "$output_git_init"
+			fi
+
+			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "New git repo initialized at remote destination: ${GITCID_NEW_REPO_PATH_HOST}:${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME}"
+		
+		else
+			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "Initializing new git repo at local destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+
+			mkdir -p ${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME}
+
+			cp -r "${GITCID_DIR}" ${GITCID_NEW_REPO_PATH_DIR}/${GITCID_NEW_REPO_NAME} >/dev/null
+			cp "${GITCID_DIR}../README.md" ${GITCID_NEW_REPO_PATH_DIR}/"${GITCID_NEW_REPO_NAME}/README-gitcid.md" 2>/dev/null || \
+			cp "${GITCID_DIR}../README-gitcid.md" ${GITCID_NEW_REPO_PATH_DIR}/"${GITCID_NEW_REPO_NAME}/README-gitcid.md" 2>/dev/null
+
+			output_git_init="$(git init --shared=${GITCID_NEW_REPO_PERMISSIONS} --template=${GITCID_INIT_TEMPLATE_DIR} ${GITCID_NEW_REPO_BARE} "${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}" 2>&1)"
+			res_git_init=$?
+			if [ $res_git_init -ne 0 ]; then
+				gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "$output_git_init"
+				gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "Failed initializing new local git repo: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+				
+				return $res_git_init
+			
+			else
+				gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "$output_git_init"
+			fi
+
+			gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "New git repo initialized at local destination: ${GITCID_NEW_REPO_PATH}/${GITCID_NEW_REPO_NAME}"
+		fi
+
+	done
+}
+
 gitcid_init() {
 	GITCID_DIR=${GITCID_DIR:-".gc/"}
 	GITCID_NEW_REPO_PERMISSIONS=${GITCID_NEW_REPO_PERMISSIONS:-"0640"}
@@ -274,52 +335,6 @@ gitcid_init() {
 		
 		return $res_make_new_git_repo
 	fi
-}
-
-gitcid_wait_for_background_jobs() {
-	for job_pid in ${GITCID_BACKGROUND_JOBS[@]}; do
-		any_jobs="y"
-		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "Waiting for GitCid background job to finish: $job_pid"
-		wait $job_pid
-	done
-
-	GITCID_BACKGROUND_JOBS=()
-
-	if [ ! -z ${any_jobs+x} ]; then
-		unset any_jobs
-		gitcid_log_info "${BASH_SOURCE[0]}" $LINENO "All GitCid background jobs are finished."
-	fi
-}
-
-gitcid_mention_update() {
-	if [ -f "${GITCID_DIR}.gc-update-available" ]; then
-		gitcid_log_update "${BASH_SOURCE[0]}" $LINENO "An updated version of GitCid is available. \
-Please run the following command at your earliest opportunity:\ngit pull" 1>&2
-		return 0
-	fi
-
-	if [ ! -z ${GITCID_CHECKED_FOR_UPDATES+x} ]; then
-		gitcid_log_update_verbose "${BASH_SOURCE[0]}" $LINENO "GitCid is up-to-date." 1>&2
-	fi
-}
-
-gitcid_init_exit() {
-	res=$1
-	shift
-
-	gitcid_wait_for_background_jobs
-
-	gitcid_mention_update
-
-	gitcid_log_notice_verbose "${BASH_SOURCE[0]}" $LINENO "GitCid script finished: $0 $@"
-
-	if [ $res -ne 0 ]; then
-		gitcid_log_err "${BASH_SOURCE[0]}" $LINENO "GitCid script finished with an error.\nexit code:\n$res"
-	else
-		gitcid_log_echo_verbose "${BASH_SOURCE[0]}" $LINENO "exit code:\n$res"
-	fi
-
-	return $res
 }
 
 gitcid_init $@
