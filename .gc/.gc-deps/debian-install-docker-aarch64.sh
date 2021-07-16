a353afe680497bc984bbefbbc35b2a62be2b965a
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-15 21:58:22 -0400

    Fix Docker install.

--- .gc/.gc-deps/debian-install-docker-aarch64.sh
+++ .gc/.gc-deps/debian-install-docker-aarch64.sh
@@ -27,7 +27,7 @@ gitcid_debian_install_docker_aarch64() {
   sudo apt-get update && \
   sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release && \
   curl -fsSL https://download.docker.com/linux/debian/gpg | \
-  sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
+  sudo gpg --batch --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
   echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
   sudo apt-get update && \
