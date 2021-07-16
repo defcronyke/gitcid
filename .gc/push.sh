73bb4ebeab5ccdf9a7c9c673d16b2d3ffc0cf3d4
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-15 23:39:31 -0400

    Add .gc/push.sh which tries to git push twice if the first attempt fails.

---
+++ .gc/push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Try to push twice, just in case the first time
+# failed for some unusual reason and might work
+# on second try.
+#
+# This is useful for example, when the remote
+# didn't have Docker installed before and has
+# it installed by GitCid, then it's needed to
+# push a second time for the user to gain its
+# newly granted docker group permissions to be
+# able to operate docker for CI/CD.
+
+gitcid_push() {
+  input="$@"
+
+  git push "$input" || \
+  git push "$input"
+}
+
+gitcid_push "$@"
