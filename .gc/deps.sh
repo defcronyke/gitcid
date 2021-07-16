734d69ed1598e9c72235c84cdb4c8c2a92a2947b
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-16 01:41:04 -0400

    Fix Docker install.

--- .gc/deps.sh
+++ .gc/deps.sh
@@ -221,7 +221,7 @@ please our system, if you know the correct values for your unsupported OS:"
         # Install docker-ce official gpg keyring.
         curl -fsSL https://download.docker.com/linux/debian/gpg > tmp-key.txt && \
         cat tmp-key.txt | \
-        sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg; \
+        yes | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg; \
         rm tmp-key.txt
 				
 				if [ $IS_X64 -eq 0 ]; then
