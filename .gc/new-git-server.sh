ab55e4745452ad104aa497e300a7f9b0f22bcce2
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-22 07:35:29 -0400

    Improve new-git-server.sh

--- .gc/new-git-server.sh
+++ .gc/new-git-server.sh
@@ -19,25 +19,55 @@
 gitcid_new_git_server_usage() {
   echo ""
   echo "Install a git server at an ssh target location."
+  echo "-------"
   echo ""
   echo "Currently supported target platforms are:"
   echo ""
   echo "  Raspberry Pi OS (aarch64)"
   echo ""
   echo ""
-  echo "usage: $0 [-y] <target new git server's ssh path>"
+  echo "usage: $0 [-y[o]] <target new git server's ssh path>"
+  echo "-----"
   echo ""
-  echo "example: $0 $USER@git1"
+  echo "Examples"
+  echo "--------"
+  echo "Interactive:"
+  echo "  $0 $USER@git1"
   echo ""
+  echo "Interactive, open web browser:"
+  echo "  $0 -o $USER@git1"
+  echo ""
+  echo "Non-interactive:"
+  echo "  $0 -y $USER@git1"
+  echo ""
+  echo "Non-interactive, open web browser:"
+  echo "  $0 -yo $USER@git1"
+  echo ""
+  echo "--------"
   echo ""
   echo "Full instructions are here:"
   echo ""
   echo "  https://gitlab.com/defcronyke/gitcid"
   echo ""
+  echo "--------"
+  echo ""
+}
+
+gc_new_git_server_interactive() {
+  echo ""
+  echo "Are you sure you want to install a git server at the ssh path: $gc_new_git_server_target [ y / N ] ? "
+
+  read gc_new_git_server_confirm
+  if [ "$gc_new_git_server_confirm" != "y" ] && [ "$gc_new_git_server_confirm" != "Y" ]; then
+    echo "Cancelled git server installation."
+    echo ""
+    return 2
+  fi
 }
 
 gitcid_new_git_server() {
   input="$@"
+  gc_new_git_server_open_web_browser=1
 
   trap 'echo ""; gitcid_new_git_server_usage; exit 255' INT
 
@@ -61,19 +91,19 @@ gitcid_new_git_server() {
   elif [ $# -lt 2 ]; then
     gc_new_git_server_target="$1"
     
-    echo ""
-    echo "Are you sure you want to install a git server at the ssh path: $gc_new_git_server_target [ y / N ] ? "
-
-    read gc_new_git_server_confirm
-    if [ "$gc_new_git_server_confirm" != "y" ] && [ "$gc_new_git_server_confirm" != "Y" ]; then
-      echo "Cancelled git server installation."
-      echo ""
-      return 2
-    fi
+    gc_new_git_server_interactive $gc_new_git_server_target || \
+    return $?
 
   else
     if [ "$1" == "-y" ]; then
       gc_new_git_server_target="$2"
+    elif [ "$1" == "-o" ]; then
+      gc_new_git_server_open_web_browser=0
+      gc_new_git_server_interactive $gc_new_git_server_target || \
+      return $?
+    elif [ "$1" == "-yo" ] || [ "$1" == "-oy" ]; then
+      gc_new_git_server_target="$2"
+      gc_new_git_server_open_web_browser=0
     else
       gitcid_new_git_server_usage
       return 1
@@ -91,12 +121,21 @@ gitcid_new_git_server() {
   echo ""
   echo "GitWeb servers detected on your network:"
   echo ""
-  .gc/git-servers-open.sh 2>/dev/null
-
-  echo ""
-  echo "GitWeb pages launched in web browser. If any pages don't"
-  echo "load on first attempt, try refreshing the page."
-  echo ""
+  if [ $gc_new_git_server_open_web_browser -eq 0 ]; then
+    .gc/git-servers-open.sh 2>/dev/null
+    echo ""
+    echo "GitWeb pages launched in web browser. If any pages don't"
+    echo "load on first attempt, try refreshing the page."
+    echo ""
+  else
+    .gc/git-servers.sh 2>/dev/null
+    echo ""
+    echo "To open a web browser tab for each detected GitWeb server,"
+    echo "run this command:"
+    echo ""
+    echo "  .gc/git-servers-open.sh"
+    echo ""
+  fi
 
   return $res
 }
