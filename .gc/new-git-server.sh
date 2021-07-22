3017fcab9b560db4a3dd72166d8c229cb52b5881
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-22 14:07:39 -0400

    Fix ssh exit.

--- .gc/new-git-server.sh
+++ .gc/new-git-server.sh
@@ -137,7 +137,7 @@ gitcid_new_git_server() {
   # stty tostop
   stty -tostop
   for j in $@; do
-    { stty -tostop; ssh -tt $j 'stty -tostop; echo ""; echo "-----"; echo "hostname: $(hostname)"; echo "-----"; curl -sL https://tinyurl.com/git-server-init | bash; exit 0' & tasks+=( $! ); exit 0; } & tasks+=( $! )
+    { stty -tostop; ssh -tt $j 'stty -tostop; echo ""; echo "-----"; echo "hostname: $(hostname)"; echo "-----"; curl -sL https://tinyurl.com/git-server-init | bash; exit 0' & tasks+=( $! ); } & tasks+=( $! )
     #  & tasks+=( $! )
   done
 
