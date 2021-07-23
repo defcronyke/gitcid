319e77df7ea47de46c30309c59722636952c3d98
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-23 09:07:48 -0400

    Fix Docker installation on armv6 platforms, such as Raspberry Pi Zero W and maybe some others.

--- .gc/.gc-util/debian-fix-docker-stuck.sh
+++ .gc/.gc-util/debian-fix-docker-stuck.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Fix docker getting stuck sometimes after install on Debian.
+#
+# TODO: Need to add a fix for Raspberry Pi Zero W, similar to:
+#
+#   sudo apt-get remove -y --purge docker-ce* container.io*; curl https://get.docker.com | sh
+#
 
 gitcid_debian_fix_docker_stuck() {
   docker ps >/dev/null 2>&1
@@ -32,10 +37,29 @@ gitcid_debian_fix_docker_stuck() {
     echo ""
     echo "trying to restart docker..."
     echo ""
-    sudo systemctl restart docker && \
-    echo "docker restart success! yay, it's fixed!"
+    sudo systemctl restart docker
+    if [ $? -ne 0 ]; then
+      echo "error: Docker is being mean. It still didn't start after that? Okay checking if maybe we are running on an older model of arm chip, such as armv6..."
+      uname -a | grep armv6
+      if [ $? -eq 0 ]; then
+        echo ""
+        echo "error: Yes, this is an armv6 type of chip. We should be able to get docker installed and running by another method then. Trying:"
+        echo ""
+        echo "sudo apt-get remove -y --purge docker-ce* container.io*; curl https://get.docker.com | sh"
+        echo ""
+        sudo apt-get remove -y --purge docker-ce* container.io*; curl https://get.docker.com | sh
+        echo ""
+      fi
+    fi
     echo ""
     docker ps >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+      echo ""
+      echo "error: Docker basic usage failed, but maybe it's installed properly by now and you just have to log out and log back in to gain Docker use privileges."
+      echo ""
+      echo "Please run whatever previous command lead to you seeing this message again, and it will hopefully work next time. Sorry for the inconvenience, Docker is a bit weird on some platforms."
+      echo ""
+    fi
     echo ""
   fi
 }
