2421c47022a5e38f79e66cadb32bb75c2a37d8b6
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-25 09:28:10 -0400

    Add "./bootstrap.sh -d" option for dev: source <(curl -sL https://tinyurl.com/gitcid) -d

--- .gc/bootstrap.sh
+++ .gc/bootstrap.sh
@@ -16,8 +16,8 @@
 
 
 gitcid_bootstrap() {
-	GITCID_OVERRIDE_REPO_TYPE=${GITCID_OVERRIDE_REPO_TYPE:-""}
-	
+  GITCID_OVERRIDE_REPO_TYPE=${GITCID_OVERRIDE_REPO_TYPE:-""}
+  
   # GitCid git repo.
   GITCID_GIT_PROJECT_SOURCE="https://gitlab.com/defcronyke/gitcid.git"
 
@@ -39,112 +39,112 @@ gitcid_bootstrap() {
   git config --global init.defaultBranch master >/dev/null 2>&1
   # ----------
 
-	if [ $# -ge 1 ]; then
+  if [ $# -ge 1 ]; then
     echo ""
     echo "args: $@"
     echo ""
 
-		new_args=()
-		for arg in "$@"; do
+    new_args=()
+    for arg in "$@"; do
       # If existing repo arg is set: -e
       printf "%b\n" "$arg" | grep -P "^\-.*e.*$|\-\-existing-repo" >/dev/null
-			# printf "%b\n" "-e" | grep -P "^\-.*e.*$|\-\-existing-repo" >/dev/null
-			if [ $? -eq 0 ]; then
-				GITCID_EXISTING_REPO="y"
+      # printf "%b\n" "-e" | grep -P "^\-.*e.*$|\-\-existing-repo" >/dev/null
+      if [ $? -eq 0 ]; then
+        GITCID_EXISTING_REPO="y"
 
-				if [ "$arg" != "--existing-repo" ]; then
-					new_arg=$(printf "%b" "$arg" | sed -E "s/^(\-.*)(e)(.*)$/\1\3/g")
+        if [ "$arg" != "--existing-repo" ]; then
+          new_arg=$(printf "%b" "$arg" | sed -E "s/^(\-.*)(e)(.*)$/\1\3/g")
 
-					if [ "$new_arg" != "-" ]; then
-						new_args+=("$new_arg")
-					fi
-				fi
-			else
-				new_args+=("$arg")
-			fi
+          if [ "$new_arg" != "-" ]; then
+            new_args+=("$new_arg")
+          fi
+        fi
+      else
+        new_args+=("$arg")
+      fi
 
       # If dev arg is set: -d
       printf "%b\n" "$arg" | grep -P "^\-.*d.*$|\-\-dev" >/dev/null
       # printf "%b\n" "-d" | grep -P "^\-.*d.*$|\-\-dev" >/dev/null
-			if [ $? -eq 0 ]; then
+      if [ $? -eq 0 ]; then
         # if [ ! -z ${GITCID_EXISTING_REPO+x} ]; then
         #   echo "error: Cannot specify command line flags -e and -d together."
         #   return 2
         # fi
 
-				GITCID_DEV_REPO="y"
+        GITCID_DEV_REPO="y"
 
-			# 	if [ "$arg" != "--dev" ]; then
-			# 		new_arg=$(printf "%b" "$arg" | sed -E "s/^(\-.*)(d)(.*)$/\1\3/g")
+      # 	if [ "$arg" != "--dev" ]; then
+      # 		new_arg=$(printf "%b" "$arg" | sed -E "s/^(\-.*)(d)(.*)$/\1\3/g")
 
-			# 		if [ "$new_arg" != "-" ]; then
-			# 			new_args+=("$new_arg")
-			# 		fi
-			# 	fi
-			# else
-			# 	new_args+=("$arg")
-			fi
-		done
+      # 		if [ "$new_arg" != "-" ]; then
+      # 			new_args+=("$new_arg")
+      # 		fi
+      # 	fi
+      # else
+      # 	new_args+=("$arg")
+      fi
+    done
 
-		set -- "${new_args[@]}"
-	fi
+    set -- "${new_args[@]}"
+  fi
 
-	if [ ! -z ${GITCID_EXISTING_REPO+x} ]; then
-		pwd="$PWD"
-		echo "note: GitCid is being installed into an existing git repo: $pwd"
+  if [ ! -z ${GITCID_EXISTING_REPO+x} ]; then
+    pwd="$PWD"
+    echo "note: GitCid is being installed into an existing git repo: $pwd"
 
-		if [ ! -d ".git" ]; then
-			printf "%b\n" "$@" | grep -P "^\-.*b.*$|\-\-bare" >/dev/null
-			if [ $? -ne 0 ] && [ -z "$GITCID_OVERRIDE_REPO_TYPE" ]; then
-				printf "%b\n" "\nwarning: The current directory doesn't have a \".git/\" folder. \
+    if [ ! -d ".git" ]; then
+      printf "%b\n" "$@" | grep -P "^\-.*b.*$|\-\-bare" >/dev/null
+      if [ $? -ne 0 ] && [ -z "$GITCID_OVERRIDE_REPO_TYPE" ]; then
+        printf "%b\n" "\nwarning: The current directory doesn't have a \".git/\" folder. \
 Assuming it's a bare repo, and treating it as such. To suppress this warning next time, \
 run the command with the following flag: -b\n\n\
 To override this automatic fix, you can set the following environment variable:\n\
 GITCID_OVERRIDE_REPO_TYPE=\"y\"\n"
 
-				new_args=()
-				for arg in "$@"; do
-					new_args+=("$arg")
-				done
+        new_args=()
+        for arg in "$@"; do
+          new_args+=("$arg")
+        done
 
-				new_args+=("-b")
+        new_args+=("-b")
 
-				set -- "${new_args[@]}"
-			fi
-		else
-			printf "%b\n" "$@" | grep -P "^\-.*b.*$|\-\-bare" >/dev/null
-			if [ $? -eq 0 ] && [ -z "$GITCID_OVERRIDE_REPO_TYPE" ]; then
-				printf "%b\n" "\nwarning: The current directory has a \".git/\" folder, but the \"-b\" flag was used. \
+        set -- "${new_args[@]}"
+      fi
+    else
+      printf "%b\n" "$@" | grep -P "^\-.*b.*$|\-\-bare" >/dev/null
+      if [ $? -eq 0 ] && [ -z "$GITCID_OVERRIDE_REPO_TYPE" ]; then
+        printf "%b\n" "\nwarning: The current directory has a \".git/\" folder, but the \"-b\" flag was used. \
 Assuming it's a normal (non-bare) repo, and treating it as such. To suppress this warning next time, \
 run the command without the following flag: -b\n\n\
 To override this automatic fix, you can set the following environment variable:\n\
 GITCID_OVERRIDE_REPO_TYPE=\"y\"\n"
 
-				new_args=()
-				for arg in "$@"; do
-					if [ "$arg" != "--bare" ]; then
-						new_arg=$(printf "%b\n" "$arg" | sed -E "s/^(\-.*)(b)(.*)$/\1\3/g")
+        new_args=()
+        for arg in "$@"; do
+          if [ "$arg" != "--bare" ]; then
+            new_arg=$(printf "%b\n" "$arg" | sed -E "s/^(\-.*)(b)(.*)$/\1\3/g")
 
-						if [ "$new_arg" != "-" ]; then
-							new_args+=("$new_arg")
-						fi
-					fi
-				done
+            if [ "$new_arg" != "-" ]; then
+              new_args+=("$new_arg")
+            fi
+          fi
+        done
 
-				set -- "${new_args[@]}"
-			fi
-		fi
+        set -- "${new_args[@]}"
+      fi
+    fi
 
-		tmpdir="$(mktemp -d)"
-		cd "$tmpdir"
+    tmpdir="$(mktemp -d)"
+    cd "$tmpdir"
     
     if [ $? -ne 0 ]; then
       echo "error: Failed entering tmp directory: $tmpdir"
       echo "error: Cannot continue, this isn't going to work, sorry! Exiting..."
       return 1
     fi
-		
-		git clone ${GITCID_GIT_PROJECT_SOURCE} && cd gitcid && echo "" && \
+    
+    git clone ${GITCID_GIT_PROJECT_SOURCE} && cd gitcid && echo "" && \
     .gc/init.sh $@ "$pwd"
 
     cd "$pwd"
@@ -158,41 +158,41 @@ GITCID_OVERRIDE_REPO_TYPE=\"y\"\n"
 
     echo ""
 
-		return 0
+    return 0
 
   elif [ ! -z ${GITCID_DEV_REPO+x} ]; then
-		echo "note: GitCid is being installed as well as any related project git repos, for development purposes."
+    echo "note: GitCid is being installed as well as any related project git repos, for development purposes."
 
     git clone ${GITCID_GIT_PROJECT_SOURCE} && cd gitcid && echo "" && \
-	  .gc/init.sh -h $@; \
+    .gc/init.sh -h $@; \
     cd ..
 
     git clone ${GITCID_GIT_PROJECT_SOURCE_GIT_SERVER} && cd git-server && echo "" && \
     source <(curl -sL https://tinyurl.com/gitcid) -e && \
     echo "" && \
-	  .gc/init.sh -h $@; \
+    .gc/init.sh -h $@; \
     cd ..; \
     echo ""
 
     git clone ${GITCID_GIT_PROJECT_SOURCE_USB_MOUNT_GIT} && cd usb-mount-git && echo "" && \
     source <(curl -sL https://tinyurl.com/gitcid) -e && \
     echo "" && \
-	  .gc/init.sh -h $@; \
+    .gc/init.sh -h $@; \
     cd ..; \
     echo ""
     
     git clone ${GITCID_GIT_PROJECT_SOURCE_DISCOVER_GIT_SERVER_DNS} && cd discover-git-server-dns && echo "" && \
     source <(curl -sL https://tinyurl.com/gitcid) -e && \
     echo "" && \
-	  .gc/init.sh -h $@; \
+    .gc/init.sh -h $@; \
     cd ..; \
     echo ""
 
     return 0
-	fi
+  fi
 
-	git clone ${GITCID_GIT_PROJECT_SOURCE} && cd gitcid && echo "" && \
-	.gc/init.sh -h $@
+  git clone ${GITCID_GIT_PROJECT_SOURCE} && cd gitcid && echo "" && \
+  .gc/init.sh -h $@
 
   echo ""
 }
