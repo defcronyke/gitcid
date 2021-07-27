80a42e3608ce4d51a7ca54c3bcc743abae2a0593
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-27 18:53:38 -0400

    Improve DNS service discovery.

--- .gc/git-servers.sh
+++ .gc/git-servers.sh
@@ -7,9 +7,10 @@ gitcid_git_servers() {
 
   if [ ! -d ".gc/discover-git-server-dns" ]; then
     git clone https://gitlab.com/defcronyke/discover-git-server-dns.git .gc/discover-git-server-dns >/dev/null 2>&1 && \
-    cd .gc/discover-git-server-dns
+    cd .gc/discover-git-server-dns 2>/dev/null || return 1
   else
-    cd .gc/discover-git-server-dns && \
+    cd .gc/discover-git-server-dns 2>/dev/null || return 2
+    git reset --hard HEAD >/dev/null 2>&1
     git pull >/dev/null 2>&1
   fi
 
