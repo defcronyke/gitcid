d544dd509178a8ceb43cc4062bb797868b4c1023
Jeremy Carter <jeremy@jeremycarter.ca>
2021-08-13 12:25:56 -0400

    Improve git server install.

--- .gc/new-git-server-hostname.sh
+++ .gc/new-git-server-hostname.sh
@@ -8,7 +8,7 @@ gc_new_git_server_hostname() {
   # You can change the default git server hostname here.
   # A number starting from 1 will be appended to the end
   # to make the complete hostname, for example: git1
-  GITCID_NEW_GIT_SERVER_HOSTNAME_PREFIX_DEFAULT="${GITCID_NEW_GIT_SERVER_HOSTNAME_PREFIX_DEFAULT:-"git"}"
+  GITCID_NEW_GIT_SERVER_HOSTNAME_PREFIX_DEFAULT=${GITCID_NEW_GIT_SERVER_HOSTNAME_PREFIX_DEFAULT:-"git"}
 
   GITCID_NEW_GIT_SERVER_HOSTNAME_PREFIX="$GITCID_NEW_GIT_SERVER_HOSTNAME_PREFIX_DEFAULT"
 
